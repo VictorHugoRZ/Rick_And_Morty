34c089a4a3c793aa21f7ce345a77173624434ada
VictorHugoRZ <v.zamoravictor@gmail.com>
2023-04-13 22:39:47 -0600

    Rick And Morty

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,9 +1,35 @@
 import Card from "./Card";
-import { connect } from 'react-redux'
+import { filterCards, orderCards } from '../redux/actions';
+import { connect, useDispatch } from 'react-redux';
+import { useState } from "react";
 
 const Favorites = ({ myFavorites }) => {
+
+    const [aux, setAux] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value));
+        setAux(!aux);
+    }
+
+    const handleFilter = (event) => {
+        dispatch(filterCards(event.target.value));
+    }
+
     return (
         <div>
+            <select onChange={handleOrder} name="selector1" id="1">
+                <option value="A">Ascendente</option>
+                <option value="D">Descendente</option>
+            </select>
+            <select onChange={handleFilter} name="selector2" id="2">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">unknown</option>
+            </select>
             {
                 myFavorites?.map(fav => {
                     return (
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./action-types";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 };
 
 export const reducer = (state = initialState, action) => {
@@ -9,13 +10,29 @@ export const reducer = (state = initialState, action) => {
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.allCharacters, action.payload],
+                allCharacters: [...state.allCharacters, action.payload]
             }
         case REMOVE_FAV:
             const removedCharacter = state.myFavorites.filter((character) => character.id !== action.payload);
             return {
                 ...state,
                 myFavorites: removedCharacter
+            }
+        case FILTER:
+            const sameGender = state.allCharacters.filter((character) => character.gender === action.payload)
+            return {
+                ...state,
+                myFavorites: sameGender
+            }
+        case ORDER:
+            const allCharactersCopy = [...state.allCharacters]
+            return {
+                ...state,
+                myFavorites:
+                state.payload === 'A'
+                ? allCharactersCopy.sort((a, b) => a.id - b.id)
+                : allCharactersCopy.sort((a, b) => b.id - a.id)
             }
         default:
             return {
